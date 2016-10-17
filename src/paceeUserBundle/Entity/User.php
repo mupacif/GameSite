ebfc1b8a5ae4da62a79ac1b3c16bd959a793ea1e
Pacific <mupacif@gmail.com>
2016-10-17 14:04:03 +0200

    V1.0.0.2 update user class to make it close to fosuserbundle

--- src/paceeUserBundle/Entity/User.php
+++ src/paceeUserBundle/Entity/User.php
@@ -3,6 +3,7 @@
 namespace paceeUserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="paceeUserBundle\Repository\UserRepository")
  */
-class User
+class User extends BaseUser
 {
     /**
      * @var int
@@ -19,17 +20,7 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 }
 
